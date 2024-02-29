fb8a4670391565daea36134f795f463994706830
Jean Varela <jean.varela1@gmail.com>
2024-02-28 22:55:14 -0300

    Criado o projeto

---
+++ src/main/java/br/com/erp/ApiErpApplication.java
@@ -0,0 +1,13 @@
+package br.com.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erp/ApiErpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
